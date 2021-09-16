a7fd3b5297d0c36c4536b2e6e8a430317c05721e
Sharmila Podury <sharmila.podury@att.com>
2021-09-15 19:59:39 -0700

    Remove special group added for password change
    Use RPC implementation for password change that
    provides metadata on user calling the rpc call.
    Define a change-password rpc call with
    current-password and new-password as it's input.
    The RPC script obtains the user from the
    VCI_RPC_METADATA environment variable, which
    would be the user making the rpc call.
    This approach allows all level users, superuser,
    admin and operators to change their password, without
    the need for a special group for updating passwords.
    
    The PAM module is also updated to prompt the user
    for current password. To verify the current password
    the PAM module requires another RPC call, verify-password,
    with current-password as the input. After verifying
    the current password, the PAM module prompts for
    'New password', and uses the change-password RPC
    call for updating the password.
    
    VRVDR-55918

--- src/pam_vyatta_passwd/pam_vyatta_passwd.c
+++ src/pam_vyatta_passwd/pam_vyatta_passwd.c
@@ -26,10 +26,123 @@
 #include <security/pam_modutil.h>
 #include <security/pam_appl.h>
 
+#include <vyatta-cfg/client/mgmt.h>
 
-char *pwdfile = "/opt/vyatta/sbin/vyatta-update-password";
-char *lu = "/opt/vyatta/sbin/lu";
-char *pwduser = "vyattapwdcfg";
+#define	VERIFY_PASSWORD	1
+#define CHANGE_PASSWORD	2
+
+static int password(pam_handle_t *pamh, struct passwd *pw,
+		    const char *cur_pw, const char *new_pw,
+		    unsigned int flag)
+{
+	struct configd_conn *conn = NULL;
+	struct configd_error err;
+	const char ns[] = "vyatta-system-login-v1";
+	char name[32];
+	char input[4096];
+
+	memset(&err, 0, sizeof(err));
+
+	if (setregid(pw->pw_gid, -1) == -1) {
+		pam_syslog(pamh, LOG_DEBUG,
+			   "password - setregid() error: %s",
+			   strerror(errno));
+		goto error;
+	}
+	if (setreuid(pw->pw_uid, -1) == -1) {
+		pam_syslog(pamh, LOG_DEBUG,
+			   "password - setreuid() error: %s",
+			   strerror(errno));
+		goto error;
+	}
+
+	conn = (struct configd_conn *)calloc(1, sizeof(struct configd_conn));
+	if (conn == NULL) {
+		pam_syslog(pamh, LOG_DEBUG,
+			   "password - configd_conn allocation failed");
+		goto error;
+	}
+	if (configd_open_connection(conn) < 0) {
+		pam_syslog(pamh, LOG_DEBUG,
+			   "password - configd_open_connection() error: %s",
+			   strerror(errno));
+		goto error;
+	}
+
+	if (flag == VERIFY_PASSWORD) {
+		strncpy(name, "verify-password", sizeof(name));
+		snprintf(input, sizeof(input),
+			 "{\"current-password\":\"%s\"}", cur_pw);
+	} else if (flag == CHANGE_PASSWORD) {
+		strncpy(name, "change-password", sizeof(name));
+		snprintf(input, sizeof(input),
+			 "{\"current-password\":\"%s\",\"new-password\":\"%s\"}",
+			 cur_pw, new_pw);
+	}
+	if (configd_call_rpc(conn, ns, name, input, &err) == NULL) {
+		pam_syslog(pamh, LOG_DEBUG,
+			   "password - configd_call_rpc() error: %s",
+			   strerror(errno));
+		if (err.text && write(STDERR_FILENO, err.text,
+				      strlen(err.text)) == -1) {
+			pam_syslog(pamh, LOG_DEBUG,
+				   "password - write() error: %s",
+				   strerror(errno));
+		}
+		goto error;
+	}
+	return 0;
+error:
+	if (err.text)
+		configd_error_free(&err);
+	if (conn) {
+		configd_close_connection(conn);
+		free(conn);
+	}
+	return -1;
+}
+
+static int user_password(pam_handle_t *pamh, const char *user,
+			 struct passwd *pw, const char *cur_pw,
+			 const char *new_pw, unsigned int flag)
+{
+	pid_t pid;
+
+	pid = fork();
+	if (pid == -1) {
+		pam_syslog (pamh, LOG_DEBUG, "password - fork() error: %s",
+			    strerror(errno));
+		return PAM_SYSTEM_ERR;
+	}
+	if (pid > 0) {
+		int status = 0;
+		pid_t rc;
+
+		while ((rc = waitpid(pid, &status, 0)) == -1 && errno == EINTR)
+			;
+		if (rc == (pid_t)-1) {
+			pam_syslog(pamh, LOG_DEBUG,
+				   "password - waitpid() error: %s",
+				   strerror(errno));
+			return PAM_SYSTEM_ERR;
+		} else if (status != 0) {
+			pam_syslog(pamh, LOG_DEBUG,
+				   "password change failed for user %s",
+				   user);
+			return PAM_AUTHTOK_ERR;
+		}
+		return PAM_SUCCESS;
+	} else {
+		if (password(pamh, pw, cur_pw, new_pw, flag) != 0) {
+			pam_syslog(pamh, LOG_DEBUG,
+				   "password change failed for user %s",
+				   user);
+			_exit(1);
+		}
+		_exit(0);
+	}
+	return PAM_SYSTEM_ERR;
+}
 
 int
 pam_sm_chauthtok(pam_handle_t *pamh, int pam_flags,
@@ -37,121 +150,49 @@ pam_sm_chauthtok(pam_handle_t *pamh, int pam_flags,
                     const char **argv __attribute__ ((unused)))
 {
 	const char *user;
-	const void *newpwd;
-	struct passwd *pwd, *pwdcfg;
-	struct stat buf;
-	pid_t pid;
-	int fd[2];
+	const void *newpw, *oldpw;
+	struct passwd *pw;
+	const char *new_pw, *cur_pw;
+	char *resp[2] = {NULL, NULL};
 	int ret;
 
-	if (stat(pwdfile, &buf) == -1) {
-		pam_syslog(pamh, LOG_DEBUG, "password - stat() error: %s",
-				strerror(errno));
-		return PAM_ABORT;
-	}
 	ret = pam_get_user(pamh, &user, NULL);
 	if (ret != PAM_SUCCESS) {
-		pam_syslog(pamh, LOG_DEBUG,
-				"password - user %s is unknown",
-				user);
+		pam_syslog(pamh, LOG_DEBUG, "password - user %s is unknown",
+			   user);
 		return ret;
 	}
-	pwd = pam_modutil_getpwnam(pamh, user);
-	if (pwd == NULL) {
+	pw = pam_modutil_getpwnam(pamh, user);
+	if (pw == NULL) {
 		pam_syslog(pamh, LOG_ERR,
-				"password - no passwd entry for user %s",
-				user);
+			   "password - no passwd entry for user %s", user);
 		return PAM_USER_UNKNOWN;
 	}
-	pwdcfg = pam_modutil_getpwnam(pamh, "vyattapwdcfg");
-	if (pwdcfg == NULL) {
-		pam_syslog(pamh, LOG_ERR,
-			"password - no passwd entry for user 'vyattapwdcfg'");
-		return PAM_USER_UNKNOWN;
-	}
-	if (!((pam_flags & PAM_UPDATE_AUTHTOK) &&
-	     pam_get_item(pamh, PAM_AUTHTOK, &newpwd) == PAM_SUCCESS))
-		return PAM_SUCCESS;
-	pam_set_item(pamh, PAM_AUTHTOK, newpwd);
-
-	if (pipe(fd)) {
-		pam_syslog (pamh, LOG_DEBUG, "password - pipe() error: %s",
-				strerror(errno));
-		return PAM_SYSTEM_ERR;
-	}
-
-	pid = fork();
-	if (pid == -1) {
-		pam_syslog (pamh, LOG_DEBUG, "password - fork() error: %s",
-				strerror(errno));
-		return PAM_SYSTEM_ERR;
-	}
-	if (pid > 0) {
-		int status = 0;
-		pid_t rc;
-		char *buf;
-
-		if (asprintf(&buf, "%s:%s", user, (const char *)newpwd) == 1) {
-			pam_syslog(pamh, LOG_DEBUG,
-					"password - asprintf() error: %s",
-					strerror(errno));
-			return PAM_SYSTEM_ERR;
-		}
-		if (write(fd[1], buf, strlen(buf)) == -1) {
-			pam_syslog(pamh, LOG_DEBUG,
-					"password - write() error: %s",
-					strerror(errno));
-			free(buf);
-			return PAM_SYSTEM_ERR;
-		}
-		free(buf);
-		close(fd[0]);
-		close(fd[1]);
-
-		while ((rc = waitpid(pid, &status, 0)) == -1 && errno == EINTR)
-			;
-		if (rc == (pid_t)-1) {
-			pam_syslog(pamh, LOG_DEBUG,
-					"password - waitpid() error: %s",
-					strerror(errno));
-			return PAM_SYSTEM_ERR;
-		} else if (status != 0) {
-			pam_syslog(pamh, LOG_DEBUG,
-					"password renewal failed for user %s",
-					user);
+	if (pam_flags & PAM_PRELIM_CHECK) {
+		if (pam_prompt(pamh, PAM_PROMPT_ECHO_OFF, &resp[0],
+				"%s", "Current password: ") != PAM_SUCCESS
+				|| resp[0] == NULL) {
+			pam_syslog (pamh, LOG_DEBUG,
+				    "password - pam_prompt() error: %s",
+				    strerror(errno));
 			return PAM_AUTHTOK_ERR;
 		}
+		pam_set_item(pamh, PAM_OLDAUTHTOK, resp[0]);
+		return user_password(pamh, user, pw, resp[0], NULL,
+				     VERIFY_PASSWORD);
+	} else if ((pam_flags & PAM_UPDATE_AUTHTOK) &&
+	     pam_get_item(pamh, PAM_AUTHTOK, &newpw) != PAM_SUCCESS) {
 		return PAM_SUCCESS;
-	} else {
-		char *args[] = { lu, "-user", (char *)pwduser, pwdfile, NULL };
-		char *env[] = { NULL };;
-
-		if (dup2(fd[0], STDIN_FILENO) == -1) {
-			pam_syslog(pamh, LOG_DEBUG,
-					"password - dup2() error: %s",
-					strerror(errno));
-			_exit(1);;
-		}
-		close(fd[1]);
-
-		if (setregid(pwdcfg->pw_gid, -1) == -1) {
-			pam_syslog(pamh, LOG_DEBUG,
-					"password - setregid() error: %s",
-					strerror(errno));
-			_exit(1);;
-		}
-		if (setreuid(pwdcfg->pw_uid, -1) == -1) {
-			pam_syslog(pamh, LOG_DEBUG,
-					"password - setreuid() error: %s",
-					strerror(errno));
-			_exit(1);;
-		}
-		execve (args[0], args, env);
-
-		pam_syslog (pamh, LOG_DEBUG,
-				"password renewal failed for user %s",
-				user);
-		_exit(1);
 	}
-	return PAM_SYSTEM_ERR;
+	pam_set_item(pamh, PAM_AUTHTOK, newpw);
+	new_pw = (const char *)newpw;
+	if (pam_get_item(pamh, PAM_OLDAUTHTOK, &oldpw) != PAM_SUCCESS) {
+		pam_syslog (pamh, LOG_DEBUG,
+			    "password - pam_get_item() on OLDAUTHTOK error: %s",
+			    strerror(errno));
+		return PAM_AUTHTOK_ERR;
+	}
+	cur_pw = (const char *)oldpw;
+	return user_password(pamh, user, pw, cur_pw, new_pw,
+			     CHANGE_PASSWORD);
 }
